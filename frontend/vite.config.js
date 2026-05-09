51e2a58ae448d26134e488a84caca870c0809bf3
gj <>
2026-05-10 02:07:13 +0530

    fixed vite congif issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      port: 5173,
-      proxy: { "/api": "http://localhost:5000" },
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+  },
 });
